40b7b1e15b65548b277aa3a04e5b4fa8d6b2bcea
Kornegy <nazark.2810@gmail.com>
2026-03-09 09:43:40 +0100

    Created AccountController for Registration, adding View for registration

---
+++ EvidenceServisnichZakazek/Controllers/AccountController.cs
@@ -0,0 +1,52 @@
+﻿namespace EvidenceServisnichZakazek.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using EvidenceServisnichZakazek.Models;
+using EvidenceServisnichZakazek.Repositories;
+using BCrypt.Net;
+
+public class AccountController : Controller
+{
+    private readonly IUserRepository _userRepository;
+    
+    public AccountController(IUserRepository userRepository)
+    {
+        this._userRepository =  userRepository;
+    }
+
+    [HttpGet]
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    public async Task<IActionResult> Register(RegisterViewModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(model);
+        }
+        
+        var existingUser = await _userRepository.GetUserByEmailAsync(model.Email);
+        if (existingUser != null)
+        {
+            ModelState.AddModelError("Email| ", "Email already exists");
+            return View(model);
+        }
+        
+        //password hashing proccess
+        string hashedPassword = BCrypt.HashPassword(model.Password);
+
+        var newUser = new Users
+        {
+            Email = model.Email,
+            FullName = model.FullName,
+            PhoneNumber = model.PhoneNumber,
+            PasswordHash = hashedPassword,
+        };
+        
+        await _userRepository.AddUserAsync(newUser);
+        
+        return RedirectToAction("Index", "Home");
+    }
+    
+}
---
+++ EvidenceServisnichZakazek/Models/RegisterViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace EvidenceServisnichZakazek.Models;
+using System.ComponentModel.DataAnnotations;
+
+public class RegisterViewModel
+{   
+    [Required(ErrorMessage = "Email is required")]
+    [EmailAddress(ErrorMessage = "Email is invalid")]
+    public string Email { get; set; }
+    
+    [Required(ErrorMessage = "FullName is required")]
+    public string FullName { get; set; }
+    
+    [Required(ErrorMessage = "PhoneNumber is required")]
+    public string PhoneNumber { get; set; }
+    
+    [Required(ErrorMessage = "Password is required")]
+    [MinLength(6, ErrorMessage = "Password must be at least 6 characters")]
+    public string Password { get; set; }
+    
+    [Required(ErrorMessage = "ConfirmPassword is required")]
+    [Compare("Password", ErrorMessage = "Passwords do not match")]
+    public string ConfirmPassword { get; set; }
+}
